4fae26773e34e7312f3887204a5d66c4469bb9e7
Bilal00211 <122523960+Bilal00211@users.noreply.github.com>
2023-02-07 12:21:58 +0100

    Update pintaemojis.java

--- src/PintaEmojis/pintaemojis.java
+++ src/PintaEmojis/pintaemojis.java
@@ -1,5 +1,10 @@
 package PintaEmojis;
 
 public class pintaemojis {
-
+   public static void main(String []args) {
+	for (int i=0; i<10; i++) {
+System.out.printf("\uD83D\uDE00");
+System.out.println("\uD83D\uDE02");
+       }
+   	}
 }
